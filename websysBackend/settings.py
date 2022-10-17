4b19b2499314c9ca2cf783a6aa450248f4b85365
kensidiangco <kensidiangco@gmail.com>
2022-10-18 01:07:49 +0800

    Removing credentials

--- websysBackend/settings.py
+++ websysBackend/settings.py
@@ -51,8 +51,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = "kensidiangco@gmail.com"
-EMAIL_HOST_PASSWORD = "bbdhjavpeeloeyyl"
+EMAIL_HOST_USER = "your_email"
+EMAIL_HOST_PASSWORD = "your_password"
 
 CORS_ORIGIN_ALLOW_ALL = True
 
